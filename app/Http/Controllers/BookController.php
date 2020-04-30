6fd318edd622578a749646ce669036ab906c1cbd
0kkun <shinji.okumoto.01@gmail.com>
2020-04-30 16:28:47 +0900

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {
-        return "Hello world";
+        $books = Book::all();
+
+        // view(表示するview, [view側で参照する際の変数名, 渡す配列])
+        return view('books/index', [
+            'books' => $books,
+        ]);
     }
 }
