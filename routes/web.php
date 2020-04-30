91184b8a7429342535e74b41949e8892fe676b3f
0kkun <shinji.okumoto.01@gmail.com>
2020-05-01 08:46:07 +0900

    Update route for create book function

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,11 @@
 Route::get('/', 'HomeController@index')->name('home');
 
 // get で /books/{id}/books にリクエストが来たら BookControllerのindexメソッドを呼びだす。最後にこのルートに名前をつけてる
-Route::get('/books/{id}/books', 'BookController@index')->name('books.index');
+Route::get('/books', 'BookController@index')->name('books.index');
+
+Route::get('/books/create', 'BookController@showCreateForm')->name('books.create');
+Route::post('/books/create', 'BookController@create');
+
+
+// Route::get('/books/{id}/edit', 'BookController@showEditForm')->name('books.edit');
+// Route::post('/books/{id}/edit', 'BookController@edit');
