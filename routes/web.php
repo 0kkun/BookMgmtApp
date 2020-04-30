955884893024ffc3074793395c267cfdf02d6831
0kkun <shinji.okumoto.01@gmail.com>
2020-04-30 15:45:40 +0900

    create route and migration file

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,6 @@
 
 
 Route::get('/', 'HomeController@index')->name('home');
+
+// get で /books/{id}/books にリクエストが来たら BookControllerのindexメソッドを呼びだす。最後にこのルートに名前をつけてる
+Route::get('/books/{id}/books', 'BookController@index')->name('books.index');
