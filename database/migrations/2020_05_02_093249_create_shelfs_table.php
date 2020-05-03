cf905d606a7fed5c756bf241efc8071fb400e373
0kkun <shinji.okumoto.01@gmail.com>
2020-05-03 12:51:17 +0900

    Create 2020_05_02_093249_create_shelfs_table.php

---
+++ database/migrations/2020_05_02_093249_create_shelfs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShelfsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shelfs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('book_id')->unsigned();
+            $table->integer('status')->default(1);
+            $table->integer('point')->default(0);
+            $table->integer('finished_amount')->default(0);
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shelfs');
+    }
+}
