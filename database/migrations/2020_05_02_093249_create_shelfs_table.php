740f28eca905f40466815969b399f7db70a7c646
0kkun <shinji.okumoto.01@gmail.com>
2020-05-04 20:23:39 +0900

    Update shelfs table for user_id

--- database/migrations/2020_05_02_093249_create_shelfs_table.php
+++ database/migrations/2020_05_02_093249_create_shelfs_table.php
@@ -16,12 +16,14 @@ class CreateShelfsTable extends Migration
         Schema::create('shelfs', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('book_id')->unsigned();
+            $table->integer('user_id')->unsigned();     
             $table->integer('status')->default(1);
             $table->integer('point')->default(0);
             $table->integer('finished_amount')->default(0);
             $table->timestamps();
 
             $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
