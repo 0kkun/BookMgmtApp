7c9537eddafc87917431c055240a9655434dc391
0kkun <shinji.okumoto.01@gmail.com>
2020-05-04 20:24:23 +0900

    Prepare data using Seeder

--- database/factories/ShelfFactory.php
+++ database/factories/ShelfFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(Shelf::class, function (Faker $faker) {
     return [
         'book_id' => $faker->numberBetween(1, 30),
+        'user_id' => $faker->numberBetween(1, 30),
         'status' => $faker->numberBetween($min = 1, $max = 3),
         'point' => $faker->numberBetween($min = 1, $max = 100),
         'finished_amount' => $faker->numberBetween($min = 1, $max = 100),
